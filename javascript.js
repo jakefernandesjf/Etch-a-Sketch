8eb7a4e0a0f50aa0d2125ed575f154bde12f2d7f
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-25 17:27:24 -0700

    Implement hover feature on 16x16 grid

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ for (let row = 0; row < 16; row++) {
     const rowContainer = document.createElement("div");
     rowContainer.style.display = "flex";
     rowContainer.style.flex = 1;
-    rowContainer.style.backgroundColor = "red";
+    rowContainer.style.backgroundColor = "grey";
     rowContainer.style.alignItems = "stretch";
     for (let col = 0; col < 16; col++) {
         let squareDiv = document.createElement("div");
@@ -16,3 +16,16 @@ for (let row = 0; row < 16; row++) {
     }
     container.appendChild(rowContainer);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        const currOpacity = Number(
+            window
+            .getComputedStyle(square)
+            .getPropertyValue("opacity")
+        );
+        square.style.opacity = (currOpacity >= 1) ? 1 : currOpacity + 0.1;
+    });
+});
