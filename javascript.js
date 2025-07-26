c619ea7303a3a0676a9ff110f081c3f7f417530f
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-25 17:04:52 -0700

    Create javascript to create sketch container

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 // javascript.js
+
+const container = document.querySelector(".container");
+
+for (let row = 0; row < 16; row++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.style.display = "flex";
+    rowContainer.style.flex = 1;
+    rowContainer.style.backgroundColor = "red";
+    rowContainer.style.alignItems = "stretch";
+    for (let col = 0; col < 16; col++) {
+        let squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        squareDiv.style.flex = "1";
+        rowContainer.appendChild(squareDiv);
+    }
+    container.appendChild(rowContainer);
+}
